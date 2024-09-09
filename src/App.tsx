6f7abadb30181cc85a5a6e18e0c5ee58b3a1d683
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-10 00:24:47 +0400

    flex all cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import "./App.css"
-import Card from "./components/Card/Card"
-import data from "../data.json"
+import "./App.css";
+import Card from "./components/Card/Card";
+import data from "../data.json";
 
 function App() {
-  
   return (
     <>
-    <h1 className="txt">Deserts</h1>
-    <div className="card-container">
-      <div>{data.map(e => <Card price={e.price} category={e.category} imgSrc={e.image.desktop}/>)}</div>
-    </div>
+      <h1 className="txt">Deserts</h1>
+      <div className="card-container">
+        {data.map((e) => (
+          <Card
+            price={e.price}
+            category={e.category}
+            imgSrc={e.image.desktop}
+          />
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
