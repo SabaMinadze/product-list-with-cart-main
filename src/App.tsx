36fed2b938bd9371ad6ca65d2faba3386fe1f0b6
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-10 01:29:04 +0400

    Add answer part

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import Card from "./components/Card/Card";
 import data from "../data.json";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <>
       <h1 className="txt">Deserts</h1>
@@ -15,11 +17,20 @@ function App() {
               category={e.category}
               name={e.name}
               imgSrc={e.image.desktop}
+              onClick={() => {
+                console.log("Hello wolrd");
+                setCount(count + 1)
+                
+              }}
             />
           ))}
         </div>
       </div>
-      <br />
+      
+
+      <div className="answer">
+        a
+      </div>
 
       <center><div className="attribution">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,7 +9,7 @@ function Card(props:any) {
         <div className="container">
         <img src={props.imgSrc} alt="error" className="product"/>
         <center><button className="btn"><img src={addToCourtLogo} 
-        alt="error" className="img2"/><span className="btntxt">Add to court</span> </button></center>
+        alt="error" className="img2"/><span className="btntxt" onClick={props.onClick}>Add to court</span> </button></center>
         <p className="txt3">{props.category}</p>
         <h2 className="txt4">{props.name}</h2>
         <h4 className="txt5">${props.price}</h4>
