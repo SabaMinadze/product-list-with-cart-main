2ee10b0039f2454f3ae5354af4d08f0e719f6c18
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-10 00:21:57 +0400

    Add code

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <>
     <h1 className="txt">Deserts</h1>
-    < div>{data.map(e => <Card price={e.price} category={e.category} imgSrc={e.image.desktop}/>)}</div>
+    <div className="card-container">
+      <div>{data.map(e => <Card price={e.price} category={e.category} imgSrc={e.image.desktop}/>)}</div>
+    </div>
     </>
   )
 }
