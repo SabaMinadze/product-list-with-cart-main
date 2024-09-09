b5091a1ba22260bc04e32ffadd209c7b340c90ae
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-09 23:37:31 +0400

    Add code

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -8,6 +8,9 @@ function Card() {
     <div className="image"></div>
     <center><button className="btn"><img src={addToCourtLogo} 
     alt="error" className="img2"/><span className="btntxt">Add to court</span> </button></center>
+    <p className="txt3">Waffle</p>
+    <h2>Waffle with Berries</h2>
+    <h2>$6.50</h2>
     </div>
     </>
   )
