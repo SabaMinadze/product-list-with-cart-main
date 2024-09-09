0cc22b540dd7290c8eb3c25c972d596344a87020
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-10 01:12:15 +0400

    Add code

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,12 @@ function App() {
           ))}
         </div>
       </div>
+      <br />
+
+      <center><div className="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+      </div></center>
     </>
   );
 }
