118c3de7335e511d54d93251244710f86094cd14
Saba <92267802+SabaMinadze@users.noreply.github.com>
2024-09-10 18:51:28 +0400

    Add more functionality

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,28 @@ import carbon from "../public/images/icon-carbon-neutral.svg"
 
 function App() {
   const [count, setCount] = useState(0)
+
   const [vaffle, setVaffle] = useState("")
   const [vanilla, setVanilla] = useState("")
   const [macaron, setMacaron] = useState("")
   const [tiramisu, setTiramisu] = useState("")
   const [pistachio, setPistachio] = useState("")
   const [lemon, setLemon] = useState("")
+  const [cake, setCake] = useState("")
   const [caramel, setCaramel] = useState("")
   const [panna , setPanna] = useState("")
+
   const [price, setPrice] = useState(0)
+
+  const [vaffleprice, setVaffleprice] = useState(0)
+  const [vanillaprice, setVanillaprice] = useState(0) 
+  const [macaronprice, setMacaronprice] = useState(0) 
+  const [tiramisuprice, setiramisuprice] = useState(0) 
+  const [pistachioprice, setpistachioprice] = useState(0) 
+  const [lemonprice, setlemonprice] = useState(0) 
+  const [cakeprice, setCakePrice] = useState(0)
+  const [caramelprice, setcaramelprice] = useState(0) 
+  const [pannaprice, setpannaprice] = useState(0)  
   
 
   return (
@@ -39,9 +52,11 @@ function App() {
                 {e.name === "Classic Tiramisu" ? setTiramisu("Classic Tiramisu") : null} 
                 {e.name === "Pistachio Baklava" ? setPistachio("Pistachio Baklava") : null} 
                 {e.name === "Lemon Meringue Pie" ? setLemon("Lemon Meringue Pie") : null}
-                {e.name === "Red Velvet Cake" ? setCaramel("Red Velvet Cake") : null}
-                {e.name === "Salted Caramel Brownie" ? setPanna("Salted Caramel Brownie") : null}
-                {e.name === "Vanilla Panna Cotta" ? setLemon("Vanilla Panna Cotta") : null}  
+                {e.name === "Red Velvet Cake" ? setCake("Red Velvet Cake") : null}
+                {e.name === "Salted Caramel Brownie" ? setCaramel("Salted Caramel Brownie") : null}
+                {e.name === "Vanilla Panna Cotta" ? setPanna("Vanilla Panna Cotta") : null}
+
+
                 {e.category === "Waffle" ? setPrice(price + e.price) : null}
                 {e.category === "Crème Brûlée" ? setPrice(price + e.price) : null}
                 {e.category === "Macaron" ? setPrice(price + e.price) : null}
@@ -51,6 +66,16 @@ function App() {
                 {e.category === "Cake" ? setPrice(price + e.price) : null}
                 {e.category === "Brownie" ? setPrice(price + e.price) : null}
                 {e.category === "Panna Cotta" ? setPrice(price + e.price) : null}
+
+                {e.category === "Waffle" ? setVaffleprice(e.price) : null}
+                {e.category === "Crème Brûlée" ? setVanillaprice(e.price) : null}
+                {e.category === "Macaron" ? setMacaronprice(e.price) : null}
+                {e.category === "Tiramisu" ? setiramisuprice(e.price) : null}
+                {e.category === "Baklava" ? setpistachioprice(e.price) : null}
+                {e.category === "Pie" ? setlemonprice(e.price) : null}
+                {e.category === "Cake" ? setCakePrice(e.price) : null}
+                {e.category === "Brownie" ? setcaramelprice(e.price) : null}
+                {e.category === "Panna Cotta" ? setpannaprice(e.price) : null}
                 
               }}
             />
@@ -62,15 +87,24 @@ function App() {
       <div className="answer">
         <h1 className="txt2">Your Cart ({count})</h1>
         <h3 className="inf">{vaffle === "Waffle with Berries" ? <h3>Waffle with Berries</h3> : null}</h3>
+        {vaffle === "Waffle with Berries" ? <h2>{vaffleprice}</h2> : null}
         <h3 className="inf">{vanilla === "Vanilla Bean Crème Brûlée" ? <h3>Vanilla Bean Crème Brûlée</h3> : null}</h3>
+        {vanilla === "Vanilla Bean Crème Brûlée" ? <h2>{vanillaprice}</h2> : null}
         <h3 className="inf">{macaron === "Macaron Mix of Five" ? <h3>Macaron Mix of Five</h3> : null}</h3>
+        {macaron === "Macaron Mix of Five" ? <h2>{macaronprice}</h2> : null}
         <h3 className="inf">{tiramisu === "Classic Tiramisu" ? <h3>Classic Tiramisu</h3> : null}</h3>
+        {tiramisu === "Classic Tiramisu" ? <h2>{tiramisuprice}</h2> : null}
         <h3 className="inf">{pistachio === "Pistachio Baklava" ? <h3>Pistachio Baklava</h3> : null}</h3>
+        {pistachio === "Pistachio Baklava" ? <h2>{pistachioprice}</h2> : null}
         <h3 className="inf">{lemon === "Lemon Meringue Pie" ? <h3>Lemon Meringue Pie</h3> : null}</h3>
-        <h3 className="inf">{caramel === "Red Velvet Cake" ? <h3>Red Velvet Cake</h3> : null}</h3>
-        <h3 className="inf">{panna === "Salted Caramel Brownie" ? <h3>Salted Caramel Brownie</h3> : null}</h3>
-        <h3 className="inf">{lemon === "Vanilla Panna Cotta" ? <h3>Vanilla Panna Cotta</h3> : null}</h3>
-        <div className="orderBox">
+        {lemon === "Lemon Meringue Pie" ? <h2>{lemonprice}</h2> : null}
+        <h3 className="inf">{cake === "Red Velvet Cake" ? <h3>Red Velvet Cake</h3> : null}</h3>
+        {cake === "Red Velvet Cake" ? <h2>{cakeprice}</h2> : null}
+        <h3 className="inf">{caramel === "Salted Caramel Brownie" ? <h3>Salted Caramel Brownie</h3> : null}</h3>
+        {caramel === "Salted Caramel Brownie" ? <h2>{caramelprice}</h2> : null}
+        <h3 className="inf">{panna === "Vanilla Panna Cotta" ? <h3>Vanilla Panna Cotta</h3> : null}</h3>
+        {panna === "Vanilla Panna Cotta" ? <h2>{pannaprice}</h2> : null}
+        <div className="orderBox">      
           {count >= 1 ? <p className="order">Order Total</p> : null}
           {count >= 1 ? <p className="count">${price}</p> : null}
         </div>
