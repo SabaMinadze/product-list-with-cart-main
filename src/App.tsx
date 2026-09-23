7d5fbc4981cc06c36d39df4ac5bab9410ecd262a
User <sabatvaazz100@gmail.com>
2026-09-23 16:52:50 +0400

    Change image directory

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 import Card from "./components/Card/Card";
 import data from "../data.json";
 import { useState } from "react";
-import empty from "../public/images/illustration-empty-cart.svg";
-import carbon from "../public/images/icon-carbon-neutral.svg";
+import empty from "../src/images/illustration-empty-cart.svg";
+import carbon from "../src/images/icon-carbon-neutral.svg";
 
 
 function App() {
